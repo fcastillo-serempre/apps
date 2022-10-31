0b77af199729171b4c73020aa98ecba2f1b8423e
Felipe Castillo <castillo.devsigner@icloud.com>
2022-10-31 10:43:55 -0500

    feat(api): routed changed to /api/v1

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,15 +1,27 @@
 import * as express from 'express';
 import * as path from 'path';
+import * as cors from 'cors';
+
 import { Message } from '@apps/api-interfaces';
 
 const CLIENT_BUILD_PATH = path.join(__dirname, '../wiki');
+const API_ROOT = '/api/v1';
 
-const app = express();
+// Express server
+const app: express.Application = express();
+
+// Public directory
 app.use(express.static(CLIENT_BUILD_PATH));
+
+// Read and parse body
+app.use(express.json());
 
 const greeting: Message = { message: 'Welcome to API!' };
 
-app.get('/api', (_, res) => {
+// API routes
+app.options('*', cors());
+
+app.get(`${API_ROOT}`, (_, res) => {
   res.send(greeting);
 });
 
