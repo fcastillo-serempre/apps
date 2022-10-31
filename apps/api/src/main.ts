5a1c1a483742b3351a8868d773c68f6239fae45a
Felipe Castillo <castillo.devsigner@icloud.com>
2022-10-31 09:57:38 -0500

    feat(api): static path join

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,20 @@
 import * as express from 'express';
+import * as path from 'path';
 import { Message } from '@apps/api-interfaces';
 
+const CLIENT_BUILD_PATH = path.join(__dirname, '../wiki');
+
 const app = express();
+app.use(express.static(CLIENT_BUILD_PATH));
 
-const greeting: Message = { message: 'Welcome to api!' };
+const greeting: Message = { message: 'Welcome to API!' };
 
-app.get('/api', (req, res) => {
+app.get('/api', (_, res) => {
   res.send(greeting);
+});
+
+app.get('*', (_, response: express.Response) => {
+  response.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html'));
 });
 
 const port = process.env.port || 3333;
